a454f87a5191d503db4147cb8d119348b7bb24c6
mlarboul <mlarboul@student.42.fr>
2021-05-26 16:39:48 +0200

    setup now download worpress file

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,3 +1,7 @@
+curl -LO https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+mv wordpress/ /var/www/
+rm latest.tar.gz
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 mkdir -p /run/php/
 /usr/sbin/php-fpm7.3
