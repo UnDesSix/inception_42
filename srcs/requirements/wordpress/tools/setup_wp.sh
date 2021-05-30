126b588eac0a929d144cf8444bd312ef564c46a5
mlarboul <mlarboul@student.42.fr>
2021-05-30 11:54:02 +0200

    changing file name

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -10,4 +10,5 @@ mv /tmp/wp-config.php /var/www/wordpress
 #	Set up daemonize to 'no' in the php-fpm.conf so i can run it properly after
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 mkdir -p /run/php/
-/usr/sbin/php-fpm7.3
+
+exec /usr/sbin/php-fpm7.3
