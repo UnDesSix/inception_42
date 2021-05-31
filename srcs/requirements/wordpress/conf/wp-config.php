aec08d1456fd91f79e67f6091dd6a322e336058d
mlarboul <mlarboul@student.42.fr>
2021-05-31 16:32:20 +0200

    use env variable in wp-config and find/replace with the bash script

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_db' );
+define( 'DB_NAME', MYSQL_DATABASE );
 
 /** MySQL database username */
-define( 'DB_USER', 'admin' );
+define( 'DB_USER', MYSQL_USER );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', MYSQL_PASSWORD );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb_service:3306' );
--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -9,6 +9,10 @@ mv /tmp/wp-config.php /var/www/wordpress
 chown -R www-data:www-data /var/www/wordpress/
 
 #	Set up daemonize to 'no' in the php-fpm.conf so i can run it properly after
+
+sed -i -e "s|MYSQL_DATABASE|'$MYSQL_DATABASE'|g" /var/www/wordpress/wp-config.php
+sed -i -e "s|MYSQL_USER|'$MYSQL_USER'|g" /var/www/wordpress/wp-config.php
+sed -i -e "s|MYSQL_PASSWORD|'$MYSQL_PASSWORD'|g" /var/www/wordpress/wp-config.php
 sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
 mkdir -p /run/php/
 
